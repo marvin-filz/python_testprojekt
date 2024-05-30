90f45c343cd31e413ab718913065b2dc06fd1860
marvin-filz <marvin.filz@googlemail.com>
2024-05-30 15:11:41 +0200

    bis Seite 46

--- test2.py
+++ test2.py
@@ -1,8 +1,33 @@
+import random
+
 print("hallo welt")
 
-testvariable=42
+test = 42
 
 print(range(20))
 
+x = random.randint(1, 10)
+print(x)
 
-x = 1
+if x < 5:
+    print("python kaput")
+else:
+    print("ales gut")
+
+liste = ["dies", "ist", 5, "eine", "liste"] + ["und", "noch", "eine"]
+print(liste)
+
+d = {"Mia": "Marvin", "Marvin": "Mia"}
+print(d["Mia"])
+print(d["Marvin"])
+d["Mia"] = "toll"
+d["Marvin"] = "noch toller"
+print(d["Mia"])
+print(d["Marvin"])
+
+name = 5.5
+Name2 = name * 2
+print(Name2)
+
+if 3 < 4:
+    print("richtig")
